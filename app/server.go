5448b966ecd6a5ca21933cbaab6c1ef15febe957
spark <victor.cortes_marquez@proton.me>
2024-02-20 20:02:36 -0600

    :sparkles: feat(server.go): add basic HTTP server functionality

--- app/server.go
+++ app/server.go
@@ -1,26 +1,43 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
-func main() {
-	fmt.Println("Logs from your program will appear here!")
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
 
+	reader := bufio.NewReader(conn)
+	request, _ := reader.ReadString('\n')
+	parts := strings.Split(request, " ")
+	if parts[1] == "/" {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else {
+		conn.Write([]byte("HTTP/1.1 404 NOT FOUND\r\n\r\n"))
+	}
+}
+
+func main() {
+
+	fmt.Println("Listening server...")
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
-	defer conn.Close()
+	defer l.Close()
 
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleConnection(conn)
 	}
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
