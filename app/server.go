27ff9fe31309bddf50db689f635deb6ab7cdc6bb
spark <victor.cortes_marquez@proton.me>
2024-04-15 20:28:34 -0600

    :bug: Fix HTTP response format in agentHandler function

--- app/server.go
+++ app/server.go
@@ -117,7 +117,7 @@ func echoHandler(conn *net.Conn, request request) {
 
 func agentHandler(conn *net.Conn, request request) {
 	contentLength := strconv.Itoa(len(request.Agent))
-	formatedString := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %s\r\n %s", contentLength, request.Agent)
+	formatedString := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %s\r\n\r\n%s", contentLength, request.Agent)
 	(*conn).Write([]byte(formatedString))
 }
 
